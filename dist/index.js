afdfb08cb1492d5b2e921a6759d3247e73e9e12a
Xblunt <andy080104@gmail.com>
2025-04-18 05:17:47 +0300

    v1.18

--- dist/index.js
+++ dist/index.js
@@ -3,31 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const database_1 = require("./database");
 const url_dao_1 = require("./db/url-dao");
+const url_1 = require("./model/url");
+const url_2 = require("./utils/url");
 async function handler(event, context) {
     try {
         await (0, database_1.initDb)();
         const urlDao = new url_dao_1.UrlDao();
         await urlDao.initTable();
-        const key = event.queryStringParameters?.key;
-        if (!key) {
+        const pathToShort = event.path;
+        if (!pathToShort) {
             return {
                 statusCode: 400,
-                body: JSON.stringify({ error: "Query parameter 'key' is required." }),
-                headers: { "Content-Type": "application/json" },
-            };
-        }
-        const existing = await urlDao.findByKey(key);
-        if (!existing) {
-            return {
-                statusCode: 404,
-                body: JSON.stringify({ error: `Key '${key}' not found` }),
+                body: JSON.stringify({ error: 'path is required' }),
                 headers: { 'Content-Type': 'application/json' }
             };
         }
+        const base62Path = (0, url_2.encodeStringToBase62)(pathToShort);
+        const url = new url_1.Url(undefined, pathToShort, base62Path);
+        await urlDao.save(url);
         return {
-            statusCode: 302,
-            headers: { Location: existing.path },
-            body: ''
+            statusCode: 200,
+            body: base62Path,
+            headers: { 'Content-Type': 'text/plain' }
         };
     }
     catch (error) {
